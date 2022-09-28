60fec57aab67ced8ce6aa0ded1db36386248d0c7
xTCry [Vladislav Kh] <wolt6@mail.ru>
2022-09-28 22:50:51 +0300

    feat(pwa): added service-worker

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import Localizer from './providers/Localizer.provider';
 import { ThemeModeProvider } from './providers/ThemeMode.provider';
 import App from './containers/App.container';
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import store from './store';
 import './index.css';
@@ -25,6 +26,11 @@ root.render(
     </Provider>
   </React.StrictMode>,
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
